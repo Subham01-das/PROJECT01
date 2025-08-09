fc455e811e4ba50aa835cfcaee924992d2763e0b
Subham01-das <dassubham82400@gmail.com>
2025-08-09 10:14:25 +0530

    MADLIB

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+# # string concatenation (how to put together strings)
+# # suppose we want to create a string that says: "subscribe to my channel"
+# youtuber = "my channel" 
+# print(f"subscribe to {youtuber}")
+adj = input("Enter an adjective: ")
+verb1 = input("Enter a verb: ")
+verb2 = input("Enter another verb: ")
+famous_person = input("Enter a famous person: ")
+madlib = f"Computer programming is so {adj}! It makes me want to {verb1} and {verb2}. I can't wait to be like {famous_person}!"
+print(madlib)
+ 
